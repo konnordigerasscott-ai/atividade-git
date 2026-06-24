10c03a4bed6de921af08ee8fab2d29af2ccbf3a7
levy <konnordigerasscott@gmail.com>
2026-06-24 15:39:01 -0300

    Funcionario.php

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -14,24 +14,24 @@ class Funcionario {
         $this->cargo = $cargo;
     }
 
-    // GETTERS
+   
     public function getNome() { return $this->nome; }
     public function getIdade() { return $this->idade; }
     public function getSalario() { return $this->salario; }
     public function getCargo() { return $this->cargo; }
 
-    // SETTERS
+    
     public function setNome($nome) { $this->nome = $nome; }
     public function setIdade($idade) { $this->idade = $idade; }
     public function setSalario($salario) { $this->salario = $salario; }
     public function setCargo($cargo) { $this->cargo = $cargo; }
 
-    // 🔥 NOVO MÉTODO
+    
     public function aumentarSalario($valor) {
         $this->salario += $valor;
     }
 
-    // 🔥 NOVO MÉTODO
+    
     public function exibirFuncionario() {
         return "Nome: {$this->nome}, Cargo: {$this->cargo}, Salário: {$this->salario}";
     }
